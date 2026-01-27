c8b728542dac6d9b18ee7832b41d255537e18f4a
AEscamilla9804 <alanmauricio.escamilla@gmail.com>
2026-01-27 11:50:08 -0600

    Create order path and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-
-
-        export default function Home() {
-          return (
-            <h1 className="text-black">Hi World in Next.js</h1>
-          )
-        }
+export default function Home() {
+  return (
+    <h1 className="text-black">Hi World in Next.js</h1>
+  )
+}
