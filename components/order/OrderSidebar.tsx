90ab1339f974622b842e1c7155b324f7e9b1ef41
AEscamilla9804 <alanmauricio.escamilla@gmail.com>
2026-01-29 10:48:18 -0600

    Create PrismaClient instance. Create public folder with images

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,6 +1,9 @@
+import prisma from "@/src/lib/prisma";
 
+export default async function OrderSidebar() {
+  const categories = await prisma.category.findMany();
+  console.log(categories);
 
-export default function OrderSidebar() {
   return (
     <aside className="md:w-72 md:h-screen bg-white">
         OrderSidebar
