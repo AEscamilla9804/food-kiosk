5969393c01f120e9d0d42823babfbf4280aa73d0
AEscamilla9804 <alanmauricio.escamilla@gmail.com>
2026-02-03 11:20:17 -0600

    Client-side data recovery and validation with Zod. User feedback with Toastify

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -4,17 +4,37 @@ import { useStore } from "@/src/store"
 import ProductDetails from "./ProductDetails";
 import { useMemo } from "react";
 import { formatCurrency } from "@/src/utils";
+import { createOrder } from "@/actions/create-order-action";
+import { OrderSchema } from "@/src/schema";
+import { toast } from "react-toastify";
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order);
   const orderTotal = useMemo(() => order.reduce((total, item) => total + item.subtotal, 0), [order]);
+
+  const handleCreateOrder = (formData: FormData) => {
+    const data = {
+      name: formData.get('name')
+    }
+
+    /** Form Validation (Client) */
+    const result = OrderSchema.safeParse(data);
+    
+    if (!result.success) {
+      result.error.issues.forEach(issue => {
+        toast.error(issue.message);
+      })
+    }
+
+    createOrder();
+  }
 
   return (
     <aside className="md:h-screen md:overflow-y-auto md:w-64 lg:w-96 p-5">
         <h1 className="text-4xl text-center font-black">My Order</h1>
 
         { order.length === 0 ? (
-          <p className="text-center my-10">The cart is empty</p>
+          <p className="text-center my-10">The order is empty</p>
         ) : (
           <div className="mt-5">
             { order.map(item => (
@@ -24,12 +44,30 @@ export default function OrderSummary() {
               />
             ))}
 
-          <p className="text-2xl mt-10 text-center">
-            Total: {''}
-            <span className="font-bold">
-              { formatCurrency(orderTotal) }
-            </span>
-          </p>
+            <p className="text-2xl mt-10 text-center">
+              Total: {''}
+              <span className="font-bold">
+                { formatCurrency(orderTotal) }
+              </span>
+            </p>
+
+            <form 
+              className="w-full mt-10 space-y-5"
+              action={handleCreateOrder}
+            >
+              <input 
+                type="text" 
+                placeholder="Your name"
+                className="bg-white border border-gray-200 p-2 w-full"
+                name="name"
+              />
+
+              <input 
+                type="submit"
+                className="p-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"  
+                value="Confirm Order"
+              />
+            </form>
           </div>
         )}
     </aside>
