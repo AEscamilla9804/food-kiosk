343013fc5cd83912d79168d47cf44c1a1ffe707e
AEscamilla9804 <alanmauricio.escamilla@gmail.com>
2026-02-04 09:58:56 -0600

    Routes /admin/orders created. Render Logo

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,19 +1,22 @@
 import prisma from "@/src/lib/prisma";
 import CategoryIcon from "../ui/CategoryIcon";
+import Logo from "../ui/Logo";
 
 export default async function OrderSidebar() {
   const categories = await prisma.category.findMany();
 
   return (
     <aside className="md:w-72 md:h-screen bg-white">
-        <nav className="mt-10">
-          { categories.map(category => (
-            <CategoryIcon 
-              key={category.id}
-              category={category}
-            />
-          ))}
-        </nav>
+      <Logo />
+
+      <nav className="mt-10">
+        { categories.map(category => (
+          <CategoryIcon 
+            key={category.id}
+            category={category}
+          />
+        ))}
+      </nav>
     </aside>
   )
 }
